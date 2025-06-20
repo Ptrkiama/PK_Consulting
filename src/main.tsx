d6a6946cc7a5559d6adb2bd8cda3f502d8fcc3b0
Peter Kiama <peterkiama16@gmail.com>
2025-06-20 19:56:24 +0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics'; // ✅ Import Vercel Analytics
+
+inject(); // ✅ Initialize Analytics
 
 createRoot(document.getElementById("root")!).render(<App />);
